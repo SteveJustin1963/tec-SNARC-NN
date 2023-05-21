8de9285c7853b3c8b7e081c4852024be8be0c12a
Stephen Justin <stevejustin1963@gmail.com>
2023-05-21 23:19:22 +1000

    Update hebb-1.c

--- hebb-1.c
+++ hebb-1.c
@@ -8,7 +8,20 @@
 #define DECAY_FACTOR_HIGH 9990 // Decay factor for neurons with high weights (0.999 in integer format)
 #define THRESHOLD 500 // Threshold to determine low-weight neurons (0.5 in integer format)
 
-// Function to update weights using the Hebbian rule
+void initialize_weights(int weights[], int num_weights, int init_value) {
+    for (int i = 0; i < num_weights; i++) {
+        weights[i] = init_value;
+    }
+}
+
+int dot_product(int vec1[], int vec2[], int len) {
+    int result = 0;
+    for (int i = 0; i < len; i++) {
+        result += (vec1[i] * vec2[i]) / 1000;
+    }
+    return result;
+}
+
 void updateWeights(int weights[], int inputs[], int output, int num_weights) {
     for (int i = 0; i < num_weights; i++) {
         int w_old = weights[i];
@@ -25,19 +38,28 @@ void updateWeights(int weights[], int inputs[], int output, int num_weights) {
 
 int main() {
     int input_pattern[NUM_INPUT_NEURONS] = {1000, 500, 200, 800, 400, 600, 900, 300};
-    int hidden_weights[NUM_HIDDEN_NEURONS] = {1000}; // Initialize hidden layer weights (1.0 in integer format)
-    int output_weights[NUM_OUTPUT_NEURONS] = {500}; // Initialize output layer weights (0.5 in integer format)
-    int hidden_output, final_output;
+    int hidden_weights[NUM_HIDDEN_NEURONS];
+    int output_weights[NUM_OUTPUT_NEURONS];
+    int hidden_output[NUM_HIDDEN_NEURONS], final_output;
+
+    initialize_weights(hidden_weights, NUM_HIDDEN_NEURONS, 1000); 
+    initialize_weights(output_weights, NUM_OUTPUT_NEURONS, 500); 
 
     // Forward pass
-    hidden_output = (input_pattern[0] * hidden_weights[0]) / 1000;
-    final_output = (hidden_output * output_weights[0]) / 1000;
+    for (int i = 0; i < NUM_HIDDEN_NEURONS; i++) {
+        hidden_output[i] = dot_product(input_pattern, hidden_weights, NUM_INPUT_NEURONS);
+    }
+    final_output = dot_product(hidden_output, output_weights, NUM_HIDDEN_NEURONS);
 
     // Calculate error signal or delta_out (depends on the specific problem)
 
     // Update weights
-    updateWeights(hidden_weights, input_pattern, final_output, NUM_HIDDEN_NEURONS);
-    updateWeights(output_weights, &hidden_output, final_output, NUM_OUTPUT_NEURONS);
+    for (int i = 0; i < NUM_HIDDEN_NEURONS; i++) {
+        updateWeights(hidden_weights, input_pattern, hidden_output[i], NUM_INPUT_NEURONS);
+    }
+    updateWeights(output_weights, hidden_output, final_output, NUM_HIDDEN_NEURONS
+
+);
 
     return 0;
 }
